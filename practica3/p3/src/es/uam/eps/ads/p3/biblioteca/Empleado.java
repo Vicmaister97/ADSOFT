d42a47aaddfd59e5eb04805070e8ed6e48faab00
victor <victorgarciacarrera@gmail.com>
2017-03-21 19:15:46 +0100

    He creado mi proyecto 21-3-17

---
+++ practica3/p3/src/es/uam/eps/ads/p3/biblioteca/Empleado.java
@@ -0,0 +1,24 @@
+package es.uam.eps.ads.p3.biblioteca;
+
+public class Empleado extends Usuario{
+	private int prestamos_simultaneos;
+	private double sancion;
+	public Empleado(String nombre) {
+		super(nombre);
+		this.setPrestamos_simultaneos(20);
+		this.setSancion(sancion);
+	}
+	public int getPrestamos_simultaneos() {
+		return prestamos_simultaneos;
+	}
+	public void setPrestamos_simultaneos(int prestamos_simultaneos) {
+		this.prestamos_simultaneos = prestamos_simultaneos;
+	}
+	public double getSancion() {
+		return sancion;
+	}
+	public void setSancion(double sancion) {
+		this.sancion = sancion;
+	}
+	
+}
